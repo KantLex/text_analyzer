8d2185d6c8ab787ea6bf9684b30473aeb245f72e
KantLex <alexhuimagic@gmail.com>
2024-10-15 21:09:53 +0800

    Dummy functions in main

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+// std::io: For input/output operations.
+// Write: To flush the output buffer.
+use std::io::{self, Write};
+
 fn main() {
-    println!("Hello, world!");
+    let text = read_input();
+
+    //Passes references &text to avoid unnecessary copying.
+    let char_count = count_characters(&text);
+    let word_count = count_words(&text);
+    let longest = longest_word(&text);
+
+    println!("Character count: {}", char_count);
+    println!("Word count: {}", word_count);
+    match longest {
+        Some(word) => println!("Longest word: '{}'", word),
+        None => println!("No words found."),
+    }
 }
+
